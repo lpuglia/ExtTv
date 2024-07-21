7161292a5c424dde1e649ef7670ca7ddc502b300
Luca <luca.puglia89@gmail.com>
2024-07-21 18:46:37 +0200

    solved issue with crash

--- exttv/src/main/java/com/android/exttv/TvApp.kt
+++ exttv/src/main/java/com/android/exttv/TvApp.kt
@@ -1,8 +1,8 @@
 import android.app.Activity
-import android.app.AlertDialog
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -30,21 +34,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.tv.foundation.lazy.list.TvLazyColumn
-import androidx.tv.foundation.lazy.list.TvLazyRow
-import androidx.tv.foundation.lazy.list.items
 import androidx.tv.material3.Card
+import androidx.tv.material3.DrawerValue
 import androidx.tv.material3.Icon
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.ModalNavigationDrawer
 import androidx.tv.material3.NavigationDrawerItem
 import androidx.tv.material3.Text
+import androidx.tv.material3.rememberDrawerState
 import coil.compose.AsyncImage
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 
-data class Movie(
+data class Card(
     val id: String,
     val title: String,
     val thumbnailUrl: String,
@@ -54,13 +57,19 @@ data class Movie(
 
 data class Section(
     val title: String,
-    val movieList: List<Movie>,
+    val movieList: List<Card>,
 )
 
 object PythonInitializer {
     private var kodi: PyObject? = null
+    lateinit var sectionList: MutableState<List<Section>>
+    lateinit var isLoading: MutableState<Boolean>
+    var sectionMap: LinkedHashMap<String, Section> = linkedMapOf()
+    var titleMap: MutableMap<String, String> = mutableMapOf(Pair("","Main Menu"))
 
-    fun init(context: Activity, sectionList: MutableState<List<Section>>): PyObject? {
+    fun init(context: Activity, sectionList: MutableState<List<Section>>, isLoading: MutableState<Boolean>): PyObject? {
+        this.sectionList = sectionList
+        this.isLoading = isLoading
         if (kodi != null) {
             return kodi
         }
@@ -79,58 +88,77 @@ object PythonInitializer {
         thread.start()
         thread.join()
 
-        kodi?.let {
-            SetSection(it, "", sectionList)
-//            SetSection(it, "?ewogICAgImFjdGlvbiI6ICJsaXZlIiwKICAgICJhcmdzIjogIiIsCiAgICAiY2hhbm5lbCI6ICJsYTciLAogICAgImV4dHJhIjogIm1vdmllIiwKICAgICJmb2xkZXIiOiB0cnVlLAogICAgImdsb2JhbHNlYXJjaCI6IGZhbHNlLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAidGh1bWJuYWlsIjogImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9rb2Rpb25kZW1hbmQvbWVkaWEvbWFzdGVyL3RoZW1lcy9kZWZhdWx0L3RodW1iX29uX3RoZV9haXIucG5nIiwKICAgICJ0aXRsZSI6ICJbQl1EaXJldHRlWy9CXSIsCiAgICAidXJsIjogImh0dHBzOi8vd3d3LmxhNy5pdCIKfQ%3D%3D", sectionList)
-//            SetSection(it, "?ewogICAgImFjdGlvbiI6ICJmaW5kdmlkZW9zIiwKICAgICJhcmdzIjogIiIsCiAgICAiY2hhbm5lbCI6ICJsYTciLAogICAgImV4dHJhIjogIm1vdmllIiwKICAgICJmb2xkZXIiOiB0cnVlLAogICAgImZvcmNldGh1bWIiOiB0cnVlLAogICAgImZ1bGx0aXRsZSI6ICJMYTciLAogICAgImdsb2JhbHNlYXJjaCI6IGZhbHNlLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAibm9fcmV0dXJuIjogdHJ1ZSwKICAgICJ0aHVtYm5haWwiOiAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2tvZGlvbmRlbWFuZC9tZWRpYS9tYXN0ZXIvbGl2ZS9sYTcucG5nIiwKICAgICJ0aXRsZSI6ICJbQl1MYTdbL0JdIiwKICAgICJ1cmwiOiAiaHR0cHM6Ly93d3cubGE3Lml0L2RpcmV0dGUtdHYiCn0%3D", sectionList)
-//            SetSection(it, "?ewogICAgImFjdGlvbiI6ICJub3ZlZGFkZXMiLAogICAgImNhdGVnb3J5IjogIk5vdml0XHUwMGUwIGluIEZpbG0iLAogICAgImNoYW5uZWwiOiAibmV3cyIsCiAgICAiY29udGV4dCI6IFsKICAgICAgICB7CiAgICAgICAgICAgICJhY3Rpb24iOiAic2V0dGluZ19jaGFubmVsIiwKICAgICAgICAgICAgImNoYW5uZWwiOiAibmV3cyIsCiAgICAgICAgICAgICJleHRyYSI6ICJwZWxpY3VsYXMiLAogICAgICAgICAgICAidGl0bGUiOiAiQ2FuYWxpIGluY2x1c2kgaW46IEZpbG0iCiAgICAgICAgfQogICAgXSwKICAgICJleHRyYSI6ICJwZWxpY3VsYXMiLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAidGh1bWJuYWlsIjogImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9rb2Rpb25kZW1hbmQvbWVkaWEvbWFzdGVyL3RoZW1lcy9kZWZhdWx0L3RodW1iX21vdmllLnBuZyIsCiAgICAidGl0bGUiOiAiRmlsbSIKfQ%3D%3D", sectionList)
-        }
+        SetSection("plugin://plugin.video.kod/")
+//            SetSection("?ewogICAgImFjdGlvbiI6ICJsaXZlIiwKICAgICJhcmdzIjogIiIsCiAgICAiY2hhbm5lbCI6ICJsYTciLAogICAgImV4dHJhIjogIm1vdmllIiwKICAgICJmb2xkZXIiOiB0cnVlLAogICAgImdsb2JhbHNlYXJjaCI6IGZhbHNlLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAidGh1bWJuYWlsIjogImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9rb2Rpb25kZW1hbmQvbWVkaWEvbWFzdGVyL3RoZW1lcy9kZWZhdWx0L3RodW1iX29uX3RoZV9haXIucG5nIiwKICAgICJ0aXRsZSI6ICJbQl1EaXJldHRlWy9CXSIsCiAgICAidXJsIjogImh0dHBzOi8vd3d3LmxhNy5pdCIKfQ%3D%3D", sectionList)
+//            SetSection("?ewogICAgImFjdGlvbiI6ICJmaW5kdmlkZW9zIiwKICAgICJhcmdzIjogIiIsCiAgICAiY2hhbm5lbCI6ICJsYTciLAogICAgImV4dHJhIjogIm1vdmllIiwKICAgICJmb2xkZXIiOiB0cnVlLAogICAgImZvcmNldGh1bWIiOiB0cnVlLAogICAgImZ1bGx0aXRsZSI6ICJMYTciLAogICAgImdsb2JhbHNlYXJjaCI6IGZhbHNlLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAibm9fcmV0dXJuIjogdHJ1ZSwKICAgICJ0aHVtYm5haWwiOiAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2tvZGlvbmRlbWFuZC9tZWRpYS9tYXN0ZXIvbGl2ZS9sYTcucG5nIiwKICAgICJ0aXRsZSI6ICJbQl1MYTdbL0JdIiwKICAgICJ1cmwiOiAiaHR0cHM6Ly93d3cubGE3Lml0L2RpcmV0dGUtdHYiCn0%3D", sectionList)
+//            SetSection("?ewogICAgImFjdGlvbiI6ICJub3ZlZGFkZXMiLAogICAgImNhdGVnb3J5IjogIk5vdml0XHUwMGUwIGluIEZpbG0iLAogICAgImNoYW5uZWwiOiAibmV3cyIsCiAgICAiY29udGV4dCI6IFsKICAgICAgICB7CiAgICAgICAgICAgICJhY3Rpb24iOiAic2V0dGluZ19jaGFubmVsIiwKICAgICAgICAgICAgImNoYW5uZWwiOiAibmV3cyIsCiAgICAgICAgICAgICJleHRyYSI6ICJwZWxpY3VsYXMiLAogICAgICAgICAgICAidGl0bGUiOiAiQ2FuYWxpIGluY2x1c2kgaW46IEZpbG0iCiAgICAgICAgfQogICAgXSwKICAgICJleHRyYSI6ICJwZWxpY3VsYXMiLAogICAgImluZm9MYWJlbHMiOiB7CiAgICAgICAgIm1lZGlhdHlwZSI6ICJtb3ZpZSIKICAgIH0sCiAgICAiaXRlbWxpc3RQb3NpdGlvbiI6IDAsCiAgICAidGh1bWJuYWlsIjogImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9rb2Rpb25kZW1hbmQvbWVkaWEvbWFzdGVyL3RoZW1lcy9kZWZhdWx0L3RodW1iX21vdmllLnBuZyIsCiAgICAidGl0bGUiOiAiRmlsbSIKfQ%3D%3D", sectionList)
 
         return kodi
     }
-}
 
-fun SetSection(kodi: PyObject, argv2: String, sectionList: MutableState<List<Section>>){
-    val runnable = Runnable {
-        Log.d("Python", argv2)
-        var newItems : List<Movie> = kodi.callAttr("run", argv2).toJava(List::class.java) as List<Movie>
-        Log.d("Python", newItems.toString())
-
-        try {
-            Handler(Looper.getMainLooper()).post {
-                val currentList = sectionList.value.toMutableList()
-                currentList[0] = Section("asd", newItems)
-                sectionList.value = currentList
+    fun SetSection(argv2: String) {
+        isLoading.value = true // Start loading indicator
+        val runnable = Runnable {
+            var new_section: Section? = null
+            if (!sectionMap.contains(argv2)){
+                val title : String = titleMap.getOrDefault(argv2, "")
+                new_section = Section(title, kodi?.callAttr("run", argv2)?.toJava(List::class.java) as List<Card>)
+                for (movie in new_section.movieList){
+                    titleMap[movie.id] = movie.title
+                }
+            }else{
+                new_section = sectionMap[argv2]
             }
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
+
+            try {
+                Handler(Looper.getMainLooper()).post {
+                    val currentList = sectionList.value.toMutableList()
+                    Log.d("Python", new_section.toString())
+                    currentList.add(new_section!!)
+                    sectionList.value = currentList
+                    isLoading.value = false // Stop loading indicator
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
         }
+        Thread(runnable).start()
     }
-    Thread(runnable).start()
 }
 
 @Composable
 fun CatalogBrowser(
     context: Activity,
-    onItemSelected: (Movie) -> Unit = {},
 ) {
-
-    var sectionList = remember {mutableStateOf(listOf(Section(title = "", movieList = listOf<Movie>())))}
+    val kodi = PythonInitializer.init(
+        context = context,
+        sectionList = remember { mutableStateOf(listOf()) },
+        isLoading = remember { mutableStateOf(false) }
+    )
     var selectedIndex by remember { mutableIntStateOf(0) }
-    var isLoading = remember{mutableStateOf(false)}
+    var selectedCards = remember { mutableStateOf(listOf<Card>()) }
+
     val items = listOf(
         "Home" to Icons.Default.Home,
         "Favourites" to Icons.Default.Favorite,
         "Settings" to Icons.Default.Settings,
     )
-    val kodi = PythonInitializer.init(context, sectionList)
-    val closeDrawerWidth = 80.dp
-    val backgroundContentPadding = 10.dp
+
+    // Create a DrawerState instance to manage the drawer state
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    // Animate the width of the drawer
+    val drawerWidth by animateDpAsState(
+        targetValue = if (drawerState.currentValue == DrawerValue.Open) 255.dp else 0.dp
+    )
+
     ModalNavigationDrawer(
+        drawerState = drawerState,
         drawerContent = {
             Column(
                 Modifier
                     .background(Color.Gray)
+                    .width(drawerWidth) // Use animated width
                     .fillMaxHeight()
                     .padding(12.dp)
                     .selectableGroup(),
@@ -139,10 +167,12 @@ fun CatalogBrowser(
             ) {
                 items.forEachIndexed { index, item ->
                     val (text, icon) = item
-
                     NavigationDrawerItem(
                         selected = selectedIndex == index,
-                        onClick = { selectedIndex = index },
+                        onClick = {
+                            selectedIndex = index
+                            // Trigger drawer state change if needed
+                        },
                         leadingContent = {
                             Icon(
                                 imageVector = icon,
@@ -156,88 +186,76 @@ fun CatalogBrowser(
             }
         }
     ) {
-        TvLazyColumn(
+        LazyColumn(
             modifier = Modifier
-                .padding(start = closeDrawerWidth + backgroundContentPadding)
                 .fillMaxWidth(),
+//        verticalArrangement = Arrangement.spacedBy(100.dp)
         ) {
-
-            items(sectionList.value) { section ->
-                if (kodi != null) {
-                    Section(
-                        section = section,
-                        isLoading = isLoading,
-                        kodi = kodi,
-                        sectionList = sectionList,
-                        onItemSelected = onItemSelected
-                    )
-                }
+            itemsIndexed(PythonInitializer.sectionList.value)  { index,section ->
+                Section(
+                    section = section,
+                    selectedCards = selectedCards,
+                    index = index,
+                )
             }
         }
-        LoadingWheel(context, isLoading)
+        LoadingWheel()
     }
 }
 
+fun <T> MutableList<T>.addOrReplace(index: Int, element: T) {
+    if (index < this.size) {
+        this[index] = element
+    } else {
+        // Add null placeholders or any other placeholder value until the index is reached
+        while (this.size < index) {
+            this.add(null as T) // You can use another placeholder if null is not appropriate
+        }
+        this.add(element)
+    }
+}
 
 @Composable
 fun Section(
     section: Section,
-    isLoading: MutableState<Boolean>,
-    kodi: PyObject,
-    sectionList: MutableState<List<Section>>,
-    onItemSelected: (Movie) -> Unit = {},
+    selectedCards: MutableState<List<Card>>,
+    index: Int,
 ) {
-    var selectedMovie by remember { mutableStateOf<Movie?>(null) }
-
-    Column(modifier = Modifier.padding(16.dp)) {
+//    var selectedMovie by remember { mutableStateOf<Card?>(null) }
+//    Column(modifier = Modifier.padding(16.dp)) {
         Text(
             text = section.title,
             style = MaterialTheme.typography.headlineSmall,
             modifier = Modifier.padding(bottom = 8.dp) // Add padding below the title
         )
-        TvLazyRow(
+        LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp), // Add start padding
-            horizontalArrangement = Arrangement.spacedBy(18.dp)
+                .padding(30.dp), // Add start padding
+            horizontalArrangement = Arrangement.spacedBy(18.dp),
         ) {
             items(section.movieList) { movie ->
-                val isSelected = selectedMovie == movie
+                val isSelected = selectedCards.value.getOrNull(index) == movie
                 MovieCard(
                     movie = movie,
                     isSelected = isSelected,
                     onClick = {
-                        if(!isLoading.value) {
-                            isLoading.value = true // Start loading indicator
-
-                            // Define your Runnable to execute on a background thread
-                            val runnable = Runnable {
-                                try {
-                                    // Update UI on the main thread
-                                    Handler(Looper.getMainLooper()).post {
-                                        selectedMovie = if (isSelected) null else movie
-                                        Log.d("Python", movie.id)
-                                        SetSection(kodi, movie.id.split("?")[1], sectionList)
-
-                                        onItemSelected(movie)
-                                        isLoading.value = false // Stop loading indicator
-                                    }
-                                } catch (e: InterruptedException) {
-                                    e.printStackTrace()
-                                }
-                            }
-                            Thread(runnable).start()
+                        if(!PythonInitializer.isLoading.value) {
+                            val currentSelectedCards = selectedCards.value.toMutableList()
+                            currentSelectedCards.addOrReplace(index, movie)
+                            selectedCards.value = currentSelectedCards
+                            PythonInitializer.SetSection(movie.id)
                         }
                     }
                 )
             }
         }
-    }
+//    }
 }
 
 @Composable
 fun MovieCard(
-    movie: Movie,
+    movie: Card,
     modifier: Modifier = Modifier,
     isSelected: Boolean = false,
     onClick: () -> Unit = {}
@@ -295,8 +313,8 @@ fun MovieCard(
 
 
 @Composable
-fun LoadingWheel(context: Activity, visible: MutableState<Boolean>) {
-    if (visible.value) {
+fun LoadingWheel() {
+    if (PythonInitializer.isLoading.value) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
--- exttv/src/main/python/kodi.py
+++ exttv/src/main/python/kodi.py
@@ -12,9 +12,9 @@ utils.download_and_extract_plugin(url, kod_folder_name, False)
 
 sys.path.append(os.path.join(utils.full_addons_path(), kod_folder_name))
 
-def run(argv2=""):
-    to_return = utils.run([f'plugin://{kod_folder_name}/', '3', argv2])
-    movie_class = jclass("Movie")
+def run(argv=""):
+    to_return = utils.run([argv.split("?")[0], '3', argv.split("?")[1] if "?" in argv else ""])
+    movie_class = jclass("Card")
     movie_list = []
     for item in to_return:
         movie_list.append(movie_class(
