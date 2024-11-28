516f4c67d0929245fb00919f0baa7cf163f5b06f
Luca <luca.puglia89@gmail.com>
2024-11-28 18:18:01 +0100

    added placeholders while player is loading the cards

--- exttv/src/main/java/com/android/exttv/ui/SectionView.kt
+++ exttv/src/main/java/com/android/exttv/ui/SectionView.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,6 +27,7 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -68,6 +71,16 @@ fun SectionView(
 ) {
     val listState = rememberLazyListState()
 
+    val placeHolderList =
+        List(4) {
+                CardItem(
+                    uri = "://placeholder",
+                    label = "Loading...",
+                    fanartUrl = "spinner",
+                    isFolder = false
+                )
+        }
+
     LazyRow(
         state = listState,
         modifier = Modifier
@@ -75,7 +88,7 @@ fun SectionView(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
         contentPadding = PaddingValues(start = 40.dp, end = 40.dp),
     ) {
-        itemsIndexed(cardList) { cardIndex, card ->
+        itemsIndexed(cardList.ifEmpty { placeHolderList }) { cardIndex, card ->
             CardView(
                 card = card,
                 sectionIndex,
@@ -156,10 +169,10 @@ fun CardView(
             Column(modifier = Modifier.height(170.dp)) {
 
                 val imageUrl = if (isNotPlayer)
-                                card.primaryArt
-                           else if (!card.isFolder && Sections.focusedCardPlayerIndex == cardIndex)
-                               card.secondaryArt
-                           else card.primaryArt
+                                    card.primaryArt
+                               else if (!card.isFolder && Sections.focusedCardPlayerIndex == cardIndex)
+                                   card.secondaryArt
+                               else card.primaryArt
 
                 SmartCardPreview(imageUrl = imageUrl,
                     isSelected = Sections.getSelectedSection(sectionIndex) == cardIndex && isNotPlayer)
@@ -258,31 +271,43 @@ fun SmartCardPreview(
         .clip(RectangleShape)
     ){
 
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .listener(onSuccess = onSuccess)
-                .build(),
-            contentDescription = contentDescription,
-            contentScale = contentScale,
-            modifier = Modifier.zIndex(0f)
-                .fillMaxWidth()
-                .height(120.dp)
-                .scale(2f)
-                .alpha(alpha)
-        )
+        if(imageUrl.endsWith("/spinner")) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier.size(58.dp)
+                )
+            }
+        }else {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .listener(onSuccess = onSuccess)
+                    .build(),
+                contentDescription = contentDescription,
+                contentScale = contentScale,
+                modifier = Modifier.zIndex(0f)
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .scale(2f)
+                    .alpha(alpha)
+            )
 
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .listener(onSuccess = onSuccess)
-                .build(),
-            contentDescription = contentDescription,
-            contentScale = contentScale,
-            modifier = Modifier.zIndex(1f)
-                .fillMaxWidth()
-                .height(120.dp)
-        )
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .listener(onSuccess = onSuccess)
+                    .build(),
+                contentDescription = contentDescription,
+                contentScale = contentScale,
+                modifier = Modifier.zIndex(1f)
+                    .fillMaxWidth()
+                    .height(120.dp)
+            )
+        }
 
         Box(modifier = Modifier
             .fillMaxSize()
